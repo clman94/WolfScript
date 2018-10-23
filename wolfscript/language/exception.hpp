1775ec958285b4d42ed2f48e1127f516553a1cdc
Michael Thompson <capnsword@gmail.com>
2018-10-22 21:37:09 -0700

    Changed exception constructors to take std::string instead

--- wolfscript/language/exception.hpp
+++ wolfscript/language/exception.hpp
@@ -2,6 +2,7 @@
 
 #include "token.hpp"
 #include <exception>
+#include <vector>
 
 namespace wolfscript::exception
 {
@@ -10,35 +11,39 @@ namespace wolfscript::exception
 struct wolf_exception :
 	std::runtime_error
 {
-	wolf_exception(const char* pMsg) :
+	wolf_exception(const std::string& pMsg) :
 		std::runtime_error(pMsg)
 	{}
-};
 
-struct tokenization_error :
-	wolf_exception
-{
-	tokenization_error(const char* pMsg) :
-		wolf_exception(pMsg)
-	{}
-
-	tokenization_error(const char* pMsg, text_position pPosition) :
-		wolf_exception(pMsg),
+	wolf_exception(const std::string& pMsg, text_position pPosition) :
+		std::runtime_error(pMsg),
 		position(pPosition)
 	{}
 
 	text_position position;
 };
 
-struct parse_error :
+struct tokenization_error :
 	wolf_exception
 {
-	parse_error(const char* pMsg) :
+	tokenization_error(const std::string& pMsg) :
 		wolf_exception(pMsg)
 	{}
 
-	parse_error(const char* pMsg, token pToken) :
-		wolf_exception(pMsg),
+	tokenization_error(const std::string& pMsg, text_position pPosition) :
+		wolf_exception(pMsg, pPosition)
+	{}
+};
+
+struct parse_error :
+	wolf_exception
+{
+	parse_error(const std::string& pMsg) :
+		wolf_exception(pMsg)
+	{}
+
+	parse_error(const std::string& pMsg, token pToken) :
+		wolf_exception(pMsg, pToken.position),
 		current_token(pToken)
 	{}
 
@@ -48,9 +53,10 @@ struct parse_error :
 struct interpretor_error :
 	wolf_exception
 {
-	interpretor_error(const char* pMsg) :
+	interpretor_error(const std::string& pMsg) :
 		wolf_exception(pMsg)
 	{}
+	std::vector<std::string> stack;
 };
 
 } // namespace wolfscript::exception
